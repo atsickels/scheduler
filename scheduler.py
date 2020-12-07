408830f77be7b03b1d72778c517bfee4469cb624
sickels0 <sickels0@iastate.edu>
2020-12-06 19:34:07 -0600

    Set up user, semester, and course objects, simple print for testing in semester, simple main for testing

--- scheduler.py
+++ scheduler.py
@@ -19,6 +19,18 @@ class Course:
         self.course_difficulty = difficulty
         self.confidence = confidence
 
+        # Optional variables default to None
+
+        self.professor_name = None
+        self.professor_difficulty = None
+        self.grading_scale = None
+
+    def set_optional_var(self, professor, difficulty, scale):
+        self.professor_name = professor
+        self.professor_difficulty = difficulty
+        self.grading_scale = scale
+
+
 
 class Semester:
 
@@ -35,12 +47,22 @@ class Semester:
             print(course.course_name, end=" ")
         print()
 
+class ExamProject:
+
+    def __init__(self, pe_name, c_name, date, time):
+        self.name = pe_name
+        self.class_name = c_name
+        self.due_date = date
+        self.time_to_complete = time
 
 
 
 if __name__ == '__main__':
     me = User("Austin", 3)
     course1 = Course("CS311", 3, 70, 50)
+    print(course1.professor_name)
+    course1.set_optional_var("Basu", 3, 60)
+    print(course1.professor_name)
     course2 = Course("CS363", 3, 30, 80)
     course3 = Course("CS321", 3, 70, 50)
     course4 = Course("CS327", 3, 70, 50)
